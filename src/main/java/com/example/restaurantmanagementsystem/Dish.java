0d9a0cbb80d003ec53d4df916e1a1ce961f671c3
Kaizer-13 <kaiseryeamin@gmail.com>
2022-04-04 22:16:42 +0600

    modified:   src/main/java/com/example/restaurantmanagementsystem/Dish.java 	modified:   src/main/java/com/example/restaurantmanagementsystem/Menu.java 	new file:   src/main/java/com/example/restaurantmanagementsystem/Offline_Customer.java 	new file:   src/main/java/com/example/restaurantmanagementsystem/Online_Customer.java 	modified:   src/main/java/com/example/restaurantmanagementsystem/Order.java 	modified:   src/main/java/com/example/restaurantmanagementsystem/Order_Food.java

--- src/main/java/com/example/restaurantmanagementsystem/Dish.java
+++ src/main/java/com/example/restaurantmanagementsystem/Dish.java
@@ -1,14 +1,14 @@
 package com.example.restaurantmanagementsystem;
 
 class Dish {
-    private String name;
-    private Integer price;
+    public String name;
+    public Integer price;
     Dish(String _name, Integer _price){
         this.name=_name;
         this.price=_price;
     }
 
-    void update(Integer new_price){
+    public void update(Integer new_price){
         this.price=new_price;
     }
 }
--- src/main/java/com/example/restaurantmanagementsystem/Menu.java
+++ src/main/java/com/example/restaurantmanagementsystem/Menu.java
@@ -6,13 +6,15 @@ import java.util.List;
 import java.util.Map;
 
 public class Menu {
-    List<Dish> all_dishes = new ArrayList<Dish>();
-    Map<String,Integer> prices =new HashMap<String,Integer>();
+    public Integer number_of_dishes=0;
+    public List<Dish> all_dishes = new ArrayList<Dish>();
+    public Map<String,Integer> prices =new HashMap<String,Integer>();
 
     public void add_dish(String name, Integer price){
         Dish new_dish = new Dish(name,price);
         all_dishes.add(new_dish);
         prices.put(name,price);
+        number_of_dishes+=1;
     }
 
     public void remove_dish(String name){
@@ -20,5 +22,21 @@ public class Menu {
         Dish dish_to_be_removed = new Dish(name,price);
         all_dishes.remove(dish_to_be_removed);
         prices.remove(name);
+        number_of_dishes-=1;
     }
+
+    public void edit_price_of_dish(String name, Integer new_price){
+        Integer price = prices.get(name);
+        Dish dish_to_be_edited = new Dish(name,price);
+        Dish current_dish;
+        for(int i=0;i<number_of_dishes;i++) {
+            current_dish = all_dishes.get(i);
+            if (current_dish.name.equals(name)) {
+                current_dish.update(new_price);
+            }
+        }
+    }
+
+    public Integer get_price(String name) {return prices.get(name);}
+
 }
